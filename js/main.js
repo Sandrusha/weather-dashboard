2814f74f100e8f1a3c55cce7b32d85e07dcfc9e6
Sandra P <47577995+Sandrusha@users.noreply.github.com>
2019-10-04 06:38:15 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const weatherDay = [{
+    day: "1", overall: "sunny", morning: {temp: "24", hum: "36", wind: "6"}, noon: {temp: "23", hum: "28", wind: "8"}, evening: {temp: "18", hum: "25", wind: "8"},
+    day: "2", overall: "cloudy", morning: {temp: "20", hum: "73", wind: "19"}, noon: {temp: "22", hum: "71", wind: "15"}, evening: {temp: "18", hum: "68", wind: "15"},
+    day: "3", overall: "mostly cloudy", morning: {temp: "22", hum: "70", wind: "14"}, noon: {temp: "25", hum: "73", wind: "16"}, evening: {temp: "18", hum: "70", wind: "13"},
+    day: "4", overall: "showers", morning: {temp: "24", hum: "70", wind: "11"}, noon: {temp: "24", hum: "73", wind: "10"}, evening: {temp: "21", hum: "68", wind: "13"},
+    day: "5", overall: "scattered showers", morning: {temp: "22", hum: "66", wind: "14"}, noon: {temp: "24", hum: "71", wind: "16"}, evening: {temp: "18", hum: "67", wind: "17"},
+    day: "6", overall: "partly cloudy", morning: {temp: "22", hum: "53", wind: "13"}, noon: {temp: "23", hum: "58", wind: "14"}, evening: {temp: "20", hum: "55", wind: "10"},
+    day: "7", overall: "partly cloudy", morning: {temp: "24", hum: "51", wind: "11"}, noon: {temp: "24", hum: "50", wind: "12"}, evening: {temp: "19", hum: "53", wind: "16"}
+}]
+
+document.addEventListener("DOMContentLoaded", function(){
+    for (i = 0; i < weatherDay.length; i++) {
+        var weather = weatherDay[i];
+
+        addWeatherDay(weather);
+    }
+})
+
+function addWeatherDay(weather){
+    var weatherTable = document.getElementById("weatherTable");
+    var weatherDayBox = document.createElement("div");
+    var weatherDayTitle = document.createElement("span");
+    weatherDayTitle.innerHTML = "Day" + " " + weatherDay[0].day;
+    var weatherDayThumbnail = document.createElement("img");
+    switch(weatherDayThumbnail) {
+        case weatherDay[0].overall === "sunny":
+            // src = './images/sunny.png';
+            src = 'http://wip.2mo.ro/sandra/homework01/images/sunny.png';
+            break;
+        case weatherDay[0].overall === "cloudy":
+            // src = './images/cloudy.png';
+            src = 'http://wip.2mo.ro/sandra/homework01/images/cloudy.png'
+            break;
+        case weatherDay[0].overall === "mostly cloudy":
+            // src = './images/mostly-cloudy.png';
+            src = 'http://wip.2mo.ro/sandra/homework01/images/mostly-cloudy.png'
+            break;
+        case weatherDay[0].overall === "showers":
+            // src = './images/showers.png';
+            src = 'http://wip.2mo.ro/sandra/homework01/images/showers.png';
+            break;
+        case weatherDay[0].overall === "scattered showers":
+            // src = './images/scattered-showers.png';
+            src = 'http://wip.2mo.ro/sandra/homework01/images/scattered-showers.png';
+            break;
+        case weatherDay[0].overall === "partly cloudy":
+            // src = './images/partly-cloudy.png';
+            src = 'http://wip.2mo.ro/sandra/homework01/images/partly-cloudy.png';
+            break;
+        default:
+            src = './images/sunny.png';
+    }
+
+    weatherDayBox.appendChild(weatherDayTitle);
+    weatherDayBox.appendChild(weatherDayThumbnail);
+    
+    weatherTable.appendChild(weatherDayBox);
+}
